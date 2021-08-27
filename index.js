df04bdc15459b275e9869e332b6b857d1f872a75
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-27 15:20:11 -0400

    Adds more queries

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const db = require('./config/connection');
 // Import questions for user
 const questions = require('./questions/questions');
 
+
 // Upon start, log "Employee Tracker" and prompt for user input
 db.connect((err) => {
     if (err) throw error;
@@ -28,6 +29,18 @@ function mainMenu() {
                 case "View all departments":
                     viewAllDepartments();
                     break;
+
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+
+                case "Quit":
+                    quit();
+                    break;
             }
 
 
@@ -40,20 +53,48 @@ function mainMenu() {
 }
 
 
-function introLog() {
-    console.log(chalk.cyan(`=========================================================================================`));
-    console.log(chalk.cyan(figlet.textSync(`Employee Tracker`, {font: "slant"})));
-    console.log(chalk.cyan(`=========================================================================================`));
-}
-
 function viewAllDepartments() {
     db.promise().query("SELECT * FROM departments")
     .then( ([rows]) => {
       console.log("\n");
       console.table(rows);
     })
-    .catch(console.log)
+    .catch(console.log("Something went wrong"))
     .then( () => mainMenu());
+}
+
+function viewAllRoles() {
+    db.promise().query("SELECT * FROM roles")
+    .then( ([rows]) => {
+      console.log("\n");
+      console.table(rows);
+    })
+    .catch(console.log("Something went wrong"))
+    .then( () => mainMenu());
+}
+
+function viewAllEmployees() {
+    db.promise().query(`
+        SELECT id, first_name, last_name, role_id 
+        FROM employees
+        `)
+    .then( ([rows]) => {
+      console.log("\n");
+      console.table(rows);
+    })
+    .catch(console.log("Something went wrong"))
+    .then( () => mainMenu());
+}
+
+function quit() {
+    db.end();
+}
+
+
+function introLog() {
+    console.log(chalk.cyan(`=========================================================================================`));
+    console.log(chalk.cyan(figlet.textSync(`Employee Tracker`, {font: "slant"})));
+    console.log(chalk.cyan(`=========================================================================================`));
 }
 
 
