2af6d66027d92b72d311027b84c3724d861a2e58
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-27 12:05:43 -0400

    Edits questions.js and imports in index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// Import inquirer package
+const inquirer = require('inquirer');
+
 // Import db connection
 const db = require('./config/connection');
+
+// Questions for user
+const questions = require('./questions/questions');
+
--- questions/questions.js
+++ questions/questions.js
@@ -1,17 +1,19 @@
 // Questions for user input
 const questions = [
     {
-        name: "selection",
+        name: "choice",
         type: "list",
         message: "What would you like to do?",
-        choices: ["View all departments",
-         "View all roles",
-         "View all employees",
-         "Add a department",
-         "Add a role",
-         "Add an employee",
-         "Update an employee's role",
-         "Quit"]
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee's role",
+            "Quit"
+        ]
     }
 ];
 
