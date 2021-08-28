6fef25abc1dd806837451348f5e1e317ee793aad
Tucker Barrett <tuckerbarrett@gmail.com>
2021-08-28 09:31:33 -0400

    Completes viewAllEmployees query

--- index.js
+++ index.js
@@ -78,12 +78,13 @@ function viewAllEmployees() {
         SELECT 
             e.id, e.first_name, e.last_name, 
             r.title, r.salary,
-            d.department_name,
-            m.first_name AS reports_to
+            d.department_name AS department,
+            CONCAT(m.first_name, ' ' ,m.last_name) AS reports_to
         FROM employees e
         JOIN roles r ON e.role_id = r.id
         JOIN departments d ON r.department_id = d.id
-        JOIN employees m ON e.manager_id = m.manager_id
+        LEFT JOIN employees m ON e.manager_id = m.id
+        ORDER BY id
         `)
     .then( ([rows]) => {
       console.log("\n");
